be9f5e89f9c22cd48307e2f3deb52ef2f520077f
Kasperi Hallinen <kasperi.hallinen@tuni.fi>
2023-04-17 12:56:37 +0300

    Add exercises 8.8-8.12, library frontend

---
+++ part8/library/frontend/src/components/Birthyear.js
@@ -0,0 +1,53 @@
+import { useMutation } from '@apollo/client'
+import { useState } from 'react'
+import Select from 'react-select'
+import { ALL_AUTHORS, EDIT_AUTHOR } from '../queries'
+
+const Birthyear = ({ authors }) => {
+  const [choice, setChoice] = useState({})
+  const [born, setBorn] = useState('')
+
+  const options = authors.map((a) => {
+    return { value: a.name, label: a.name }
+  })
+
+  const [editAuthor] = useMutation(EDIT_AUTHOR, {
+    refetchQueries: [{ query: ALL_AUTHORS }],
+  })
+
+  const submit = (event) => {
+    event.preventDefault()
+
+    console.log('edit author...')
+    editAuthor({ variables: { name: choice.value, born: Number(born) } })
+
+    setChoice({})
+    setBorn('')
+  }
+
+  return (
+    <div>
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          <Select
+            value={choice}
+            options={options}
+            onChange={(target) => setChoice(target)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type='number'
+            value={born}
+            onChange={({ target }) => setBorn(target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
+    </div>
+  )
+}
+
+export default Birthyear
